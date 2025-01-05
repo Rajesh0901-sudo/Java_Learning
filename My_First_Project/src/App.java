d11a82865144269408eeeaffc1c060602b565a8e
Rajesh Sahu <rajesh09011999@gmail.com>
2025-01-06 00:31:32 +0530

    package structure

--- My_First_Project/src/App.java
+++ My_First_Project/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+import com.newProject.firstPackage.Math;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner myObj = new Scanner(System.in); 
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
+
+        Math m = new Math();
+        m.printData();
     }
 }
---
+++ My_First_Project/src/com/newProject/firstPackage/Math.java
@@ -0,0 +1,11 @@
+package com.newProject.firstPackage;
+
+
+
+public class Math {
+
+    public void printData(){
+        System.out.println("This is Math class");
+    }
+    
+}
